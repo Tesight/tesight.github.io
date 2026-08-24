b847b6f2a530d38a9207b8e5f94586b83d9bbce4
leo-191 <leo-191@users.noreply.github.com>
2026-08-24 08:34:06 +0000

    deploy: 4cc90e54b8012bd6e75cd0dcb50c33def643630a

---
+++ app/track-editor/wasm/track-core-wasm/track_core_wasm_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const buildTemplateFile: (a: any) => [number, number, number];
+export const buildTrack: (a: any) => [number, number, number, number];
+export const gcjToWgsCoord: (a: any) => [number, number, number];
+export const instantiateTemplateTrack: (a: any) => [number, number, number];
+export const loadTemplateJson: (a: any) => [number, number, number];
+export const previewTrack: (a: any) => [number, number, number];
+export const rebaseStart: (a: any) => [number, number, number];
+export const resolveWaypoint: (a: any) => [number, number, number];
+export const wgsToGcjCoord: (a: any) => [number, number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
